bb8538160d86cf83d99e529b980e371c511591f1
nemmtor <kacper0witas@gmail.com>
2020-10-30 21:07:37 +0100

    Adds heading and subHeading for 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,10 +4,10 @@ import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout headingText="Oops..." subHeading="The page you were looking for doesn't exist...">
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a route that doesn't exist... the sadness.</p>
   </Layout>
 );
 
